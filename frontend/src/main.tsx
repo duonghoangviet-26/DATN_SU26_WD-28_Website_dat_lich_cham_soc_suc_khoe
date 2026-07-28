88b5d1cb1f2e6943a7b760697f85ded1454a83ab
duonghv <hoangvietduong07052006@gmail.com>
2026-07-29 01:29:24 +0700

    thêm googleClientId và logout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App'
 import { AuthProvider } from '@/context/AuthContext'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '435851330570-6llvi4mraphakqn7os9qapu7egruu4vb.apps.googleusercontent.com'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
